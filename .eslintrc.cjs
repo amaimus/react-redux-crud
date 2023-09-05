91a2f3efebb53342ea5446dfa44897f1d1fdf927
Alejandro M <alejandromaimus147@gmail.com>
2023-09-05 19:56:49 +0200

    base template

---
+++ .eslintrc.cjs
@@ -0,0 +1,39 @@
+module.exports = {
+    env: {
+        browser: true,
+        es2021: true
+    },
+    extends: [
+        "standard-with-typescript",
+        "plugin:react/recommended"
+    ],
+    overrides: [
+        {
+            env: {
+                node: true
+            },
+            files: [
+                ".eslintrc.{js,cjs}"
+            ],
+            parserOptions: {
+                sourceType: "script"
+            }
+        }
+    ],
+    parserOptions: {
+        ecmaVersion: "latest",
+        sourceType: "module",
+        project: './tsconfig.json',
+    },
+    plugins: [
+        "react"
+    ],
+    rules: {
+        "@typescript-eslint/explicit-function-return-type": 'off',
+        "react/react-in-jsx-scope": 'off',
+        "@typescript-eslint/no-non-null-assertion": "off",
+        "@typescript-eslint/triple-slash-reference": "off",
+        indent: ["error", 2],
+        "jsx-quotes": ["error", "prefer-single"]
+    }
+}
---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import './App.css'
+
+function App () {
+  return (
+    <>
+      <h1>Base template</h1>
+    </>
+  )
+}
+
+export default App
