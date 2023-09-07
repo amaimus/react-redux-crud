0dd6ff24d9ec9ad99f631a8fa6b73c1169333258
Alejandro M <alejandromaimus147@gmail.com>
2023-09-07 15:51:59 +0200

    sonner to show status updates

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,16 +1,42 @@
-import { configureStore } from '@reduxjs/toolkit'
-import usersReducer from './users/slice'
+import { type Middleware, configureStore } from '@reduxjs/toolkit'
+import usersReducer, { rollbackUser } from './users/slice'
+import { toast } from 'sonner'
 
 const persistanceLocalStorageMiddleware = (store) => (next) => (action) => {
   next(action)
   localStorage.setItem('__redux__state__', JSON.stringify(store.getState()))
 }
 
+const syncWithDatabase: Middleware = store => next => action => {
+  const { type, payload } = action
+  const previousState = store.getState()
+
+  next(action)
+
+  if (type === 'users/deleteUserById') {
+    const userIdToRemove = payload
+    const userToRemove = previousState.users.find(user => user.id === userIdToRemove)
+
+    fetch(`https://jsonplaceholder.typicode.com/users/${userIdToRemove}`, {
+      method: 'DELETE'
+    })
+      .then(res => {
+        if (res.ok) toast.success('User removed')
+      })
+      .catch(() => {
+        toast.error(`Error deleting user ${action.payload}`)
+
+        if (userToRemove !== -1) store.dispatch(rollbackUser(userToRemove))
+        console.log('error')
+      })
+  }
+}
+
 export const store = configureStore({
   reducer: {
     users: usersReducer
   },
-  middleware: [persistanceLocalStorageMiddleware]
+  middleware: [persistanceLocalStorageMiddleware, syncWithDatabase]
 })
 
 export type RootState = ReturnType<typeof store.getState>
--- src/store/users/slice.ts
+++ src/store/users/slice.ts
@@ -56,9 +56,15 @@ export const usersSlice = createSlice({
     deleteUserById: (state, action: PayloadAction<UserId>) => {
       const id = action.payload
       return state.filter(user => user.id !== id)
+    },
+    rollbackUser: (state, action: PayloadAction<UserWithId>) => {
+      const isUserAlreadyDefined = state.some(user => user.id === action.payload.id)
+      if (!isUserAlreadyDefined) {
+        return [...state, action.payload]
+      }
     }
   }
 })
 
 export default usersSlice.reducer
-export const { deleteUserById, addNewUser } = usersSlice.actions
+export const { deleteUserById, addNewUser, rollbackUser } = usersSlice.actions
